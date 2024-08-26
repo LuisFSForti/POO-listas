6551597b23d3b52820f73f12f4cd0dcd211b25aa
LuisFSForti <luisforti@usp.br>
2024-08-26 00:13:34 -0300

    Ex10-tentativa_1

---
+++ lista01/ex10.cpp
@@ -0,0 +1,40 @@
+//Luis Filipe Silva Forti - 14592348
+
+#include <iostream>
+
+int main() {
+    double custo, valor, troco;
+
+    std::cin >> custo;
+    std::cin >> valor;
+
+    troco = valor - custo;
+
+    int notas[] = {20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1}; //Valores *100 para calcular com inteiros
+    int *qtdCedulas = (int*) calloc(sizeof(notas)/sizeof(float), sizeof(*qtdCedulas));
+
+    int atual = 0;
+
+    int aux = troco * 100; //Transforma em inteiro para nao ocorrerem erros
+
+    while (aux > 0)
+    {
+        if(aux >= notas[atual])
+        {
+            aux -= notas[atual];
+            (*(qtdCedulas + atual))++;
+        }
+        else
+            atual++;
+    }
+
+    //Define a quantidade de casas deciamais a serem imprimidas SEMPRE
+    std::cout.precision(2);
+    std::cout << std::fixed;
+
+    std::cout << "Troco: R$ " << troco << std::endl;
+
+    for (int i = 0; i < sizeof(notas)/sizeof(float); i++)
+        std::cout << *(qtdCedulas + i) << " de R$ " << (float)notas[i]/100 << std::endl;
+    return 0;
+}
