ae4a52ae2710f7f2b4cf5f10711b3851ae9a1748
LuisFSForti <luisforti@usp.br>
2024-08-25 22:54:15 -0300

    Ex4-tentativa_1

---
+++ lista01/ex4.cpp
@@ -0,0 +1,15 @@
+//Luis Filipe Silva Forti - 14592348
+
+#include <iostream>
+
+int main() {
+    int nro;
+    std::cin >> nro;
+
+    for (int i = 1; i < nro; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0 && i % 17 == 0)
+            std::cout << i << std::endl;
+    }
+    return 0;
+}
