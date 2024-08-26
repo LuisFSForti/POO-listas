555c03ad6ee7f6f636c8bdd75f7a0692affd1707
LuisFSForti <luisforti@usp.br>
2024-08-25 23:38:12 -0300

    Ex8-tentativa_1

---
+++ lista01/ex8.cpp
@@ -0,0 +1,28 @@
+//Luis Filipe Silva Forti - 14592348
+
+#include <iostream>
+#include <fstream>
+
+std::string conteudo(std::string nome)
+{
+    std::ifstream arquivo(nome);
+
+    std::string cont, aux;
+
+    while (getline (arquivo, aux))
+    {
+        cont += aux;
+    }
+
+    arquivo.close();
+    return cont;
+}
+
+int main() {
+    std::string nome;
+
+    std::cin >> nome;
+
+    std::cout << conteudo(nome) << std::endl;
+    return 0;
+}
