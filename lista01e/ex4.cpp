a21ead418c00b467f4841a391f6567dc9f5174cd
LuisFSForti <luisforti@usp.br>
2024-08-27 20:40:11 -0300

    Lista1e-ex4-tentativa_1

---
+++ lista01e/ex4.cpp
@@ -0,0 +1,23 @@
+//Luis Filipe Silva Forti - 14592348
+
+#include <iostream>
+
+int fib(unsigned long long int n, int m)
+{
+    if(n <= 2)
+        return 1;
+
+    return (fib(n-1, m) + fib(n-2, m)) % m;
+}
+
+int main() {
+    unsigned long long int n;
+    int m;
+
+    std::cin >> n;
+    std::cin >> m;
+
+    std::cout << fib(n, m) << std::endl;
+
+    return 0;
+}
