2762e87256cc441c04fe4134f9ee6dc42cc89ec4
LuisFSForti <133523565+LuisFSForti@users.noreply.github.com>
2024-08-27 17:42:49 -0300

    Ex5-tenteativa_2

--- lista01/ex5.cpp
+++ lista01/ex5.cpp
@@ -3,42 +3,14 @@
 #include <iostream>
 
 int main() {
-    char dados[1000000];
-    int qtd = 0;
-    std::string aux;
+    int val, aux;
 
-    while (std::getline(std::cin, aux))
+    while (std::cin >> aux)
     {
-        dados[qtd] = stoi(aux)+48;
-        qtd++;
+        val = val ^ aux;
     }
 
-    for(int i = 0; i < qtd; i++)
-    {
-        for(int j = i+1; j < qtd; j++)
-        {
-            if(!(dados[i] ^ dados[j]))
-            {
-                //std::cout << dados[i] << " " << i << "@@@@" << j << " " << dados[j] << std::endl; //duplicata
-                for(int a = i; a < j-1; a++)
-                {
-                    //std::cout << dados[a] << " dupla 1 " << dados[a+1] << std::endl; //duplicata
-                    dados[a] = dados[a+1];
-                }
-                for(int a = j-1; a < qtd-1; a++)
-                {
-                    //std::cout << dados[a] << " dupla 2 " << dados[a+2] << std::endl; //duplicata
-                    dados[a] = dados[a+2];
-                }
-                qtd--;
-                i--;
-                break;
-            }
-        }
-    }
-
-    std::cout << dados[0] << std::endl;
-
+    std::cout << val << std::endl;
 
     return 0;
 }
