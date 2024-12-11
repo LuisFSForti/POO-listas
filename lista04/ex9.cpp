b5eaba5cfe7ccf5fefa3f0374f31a62d3ae40acf
LuisFSForti <luisforti@usp.br>
2024-12-11 19:48:23 -0300

    Ex9 - tentativa 1

---
+++ lista04/ex9.cpp
@@ -0,0 +1,20 @@
+//Luis Filipe Silva Forti - 14592348
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+std::vector<double> notas(std::string s)
+{
+    std::string aux = s;
+    std::vector<double> ret;
+
+    while(aux.find(",") != -1)
+    {
+        aux = aux.substr(aux.find(",") + 1);
+        ret.insert(ret.end(), stod(aux.substr(0, aux.find(","))));
+        aux = aux.substr(aux.find(",") + 1);
+    }
+
+    return ret;
+}
