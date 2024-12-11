5577734ddb548b8e0caee9ae844edef2a02f4e59
LuisFSForti <luisforti@usp.br>
2024-12-11 17:59:24 -0300

    Ex6 - tentativa1

---
+++ lista04/ex6.cpp
@@ -0,0 +1,13 @@
+//Luis Filipe Silva Forti - 14592348
+
+#include <stdio.h>
+#include <vector>
+#include <set>
+
+std::vector<int> retira(std::vector<int> v, int n)
+{
+    std::set<int> ret(v.begin(), v.end());
+    ret.erase(ret.find(n));
+
+    return std::vector<int>(ret.begin(), ret.end());
+}
